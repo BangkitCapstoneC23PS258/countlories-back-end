2ea09ed28f0c0aec6983eeb9ecf571253aa6c625
Qin <70939623+qintharaandini@users.noreply.github.com>
2023-06-16 20:00:25 +0700

    Index Update

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(3000,() => console.log('Server is running on port 3000'));
+app.listen(8080,() => console.log('Server is running on port 8080'));
